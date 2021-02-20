751e1845a8d7c91aa75d7269d8f5d7ed7b6e1e3f
Natarkin Sergey <snatarkin@gmail.com>
2021-02-20 15:20:13 +0300

    added enumeration defines the type of the operand, priority and its function.

--- src/main/java/RPN/OperationType.java
+++ src/main/java/RPN/OperationType.java
@@ -8,28 +8,28 @@ import java.util.function.BiFunction;
 
 public enum OperationType {
 
-    ADDITION('+', 1, (a, b) -> a + b),
-    SUBTRACTION('-', 1, (a, b) -> a - b),
-    MULTIPLICATION('*', 2, (a, b) -> a * b),
-    DIVISTION('/', 2, (a, b) -> a / b);
+    ADDITION("+", 1, (a, b) -> a + b),
+    SUBTRACTION("-", 1, (a, b) -> a - b),
+    MULTIPLICATION("*", 2, (a, b) -> a * b),
+    DIVISTION("/", 2, (a, b) -> a / b);
 
 
-    private static final Map<Character, OperationType> ENUM_OPERATION;
+    private static final Map<String, OperationType> ENUM_OPERATION;
 
     static {
-        Map<Character, OperationType> map = new ConcurrentHashMap<>();
+        Map<String, OperationType> map = new ConcurrentHashMap<>();
         for (OperationType instance : OperationType.values()) {
             map.put(instance.operand, instance);
         }
         ENUM_OPERATION = Collections.unmodifiableMap(map);
     }
 
-    private final char operand;
+    private final String operand;
     private final int priority;
     private final BiFunction<Integer, Integer, Integer> function;
 
 
-    OperationType(char operand, int priority, BiFunction<Integer, Integer, Integer> function) {
+    OperationType(String operand, int priority, BiFunction<Integer, Integer, Integer> function) {
         this.operand = operand;
         this.priority = priority;
         this.function = function;
@@ -38,13 +38,13 @@ public enum OperationType {
     public static OperationType getOperand(Character operand) {
         return ENUM_OPERATION.get(operand);
     }
-    public static int getPriority(Character operand){
+    public static int getPriority(String operand){
         return ENUM_OPERATION.get(operand).priority;
     }
     public BiFunction<Integer, Integer, Integer> getFunction() {
         return function;
     }
-    public static boolean IsOperator(Character operand){
+    public static boolean IsOperator(String operand){
         return ENUM_OPERATION.containsKey(operand);
     }
 
