ea8738082cd0798ad17969565050747f666fb713
Natarkin Sergey <snatarkin@gmail.com>
2021-02-21 19:57:16 +0300

    changed the name of the directory

--- src/main/java/RPN/ArgumentControllerImpl.java
+++ src/main/java/RPN/ArgumentControllerImpl.java
@@ -1,4 +1,4 @@
-package RPN;
+package rpn;
 
 import java.util.Arrays;
 import java.util.regex.Pattern;
--- src/main/java/RPN/OperationType.java
+++ src/main/java/RPN/OperationType.java
@@ -1,4 +1,4 @@
-package RPN;
+package rpn;
 
 import java.util.Collections;
 import java.util.Map;
