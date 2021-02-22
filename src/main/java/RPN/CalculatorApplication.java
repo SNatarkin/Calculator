708a08e8a3e40a8f2ef5534c793caa91a39992c3
Natarkin Sergey <snatarkin@gmail.com>
2021-02-22 19:54:54 +0300

    added a class that works with the console

--- src/main/java/RPN/CalculatorApplication.java
+++ src/main/java/RPN/CalculatorApplication.java
@@ -4,15 +4,16 @@ import lombok.RequiredArgsConstructor;
 
 import java.util.Scanner;
 @RequiredArgsConstructor
-public class CalculatorApp {
+public class CalculatorApplication {
 
     private final ICalculator calculator;
-    public CalculatorApp() {
+    public CalculatorApplication() {
         this.calculator = new EvaluationCalculator();
     }
     void run() {
         Scanner sc = new Scanner(System.in);
         while (true) {
+            System.out.println("");
             String input = sc.nextLine();
             if (input.equalsIgnoreCase("exit")) {
                 System.out.println("Thanks for using the calculator, goodbye");
@@ -21,10 +22,9 @@ public class CalculatorApp {
             try {
                double result =  calculator.calculate(input);
                 System.out.println("Результат " + result);
+                System.out.println("if you want to end the program enter: exit");
 
-            } catch (ArithmeticException e) {
-                System.out.println(e.getMessage());
-            } catch (IllegalArgumentException e){
+            } catch (ArithmeticException | IllegalArgumentException e) {
                 System.out.println(e.getMessage());
             }
         }
