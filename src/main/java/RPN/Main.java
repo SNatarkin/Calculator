7766febfa6bba7a28d0fceed5aace4514dd55ca1
Natarkin Sergey <snatarkin@gmail.com>
2021-02-22 19:56:02 +0300

    added an access point to the program

--- src/main/java/RPN/Main.java
+++ src/main/java/RPN/Main.java
@@ -1,16 +1,9 @@
 package RPN;
 
 
-import java.util.Scanner;
-
 public class Main {
-
-
     public static void main(String[] args) {
-        new CalculatorApp().run();
-
-
-
+        new CalculatorApplication().run();
 
 
     }
